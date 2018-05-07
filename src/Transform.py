142a2892596002f7dfcfc146f1c9edccd3fbe10e
FrankJohansson <39047902+FrankJohansson@users.noreply.github.com>
2018-05-07 15:30:22 +0200

    Cleaned it up a bit
    Just realized that inverse does not return the original so I'll have to check that

--- src/Transform.py
+++ src/Transform.py
@@ -9,25 +9,17 @@ import numpy as np
 from scipy import *
 
 def transform(matrix, weight = sqrt(2)):
-    orthagonal = (weight == sqrt(2))
-    factor = weight/2.
-    W = Matrix(np.zeros(matrix.shape))
-    i,j = matrix.indices(W.shape)
-    W[2*i==j] = factor
-    W[2*i==j-1] = factor
-    W[2*i-W.shape[0]==j] = -factor
-    W[2*i-W.shape[0]==j-1] = factor
-    if orthagonal:
-        Winverse = W.transpose()
-    else:
-        Winverse = W.inverse()
+    W, Winverse = wavelet(matrix, weight, orthagonal = (weight == sqrt(2)))
     return W * matrix * Winverse
 
 def inverse_transform(matrix, weight = sqrt(2)):
-    orthagonal = (weight == sqrt(2))
-    factor = weight/2.
+    W, Winverse = wavelet(matrix, weight, orthagonal = (weight == sqrt(2)))
+    return Winverse * matrix * W
+
+def wavelet(matrix, weight, orthagonal):
     W = Matrix(np.zeros(matrix.shape))
     i,j = matrix.indices(W.shape)
+    factor = weight/2.
     W[2*i==j] = factor
     W[2*i==j-1] = factor
     W[2*i-W.shape[0]==j] = -factor
@@ -36,4 +28,4 @@ def inverse_transform(matrix, weight = sqrt(2)):
         Winverse = W.transpose()
     else:
         Winverse = W.inverse()
-    return Winverse * matrix * W
+    return W, Winverse
