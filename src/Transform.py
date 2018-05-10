3dea6d12801224ca62ad5f8ae9418dd242311194
FrankJohansson <39047902+FrankJohansson@users.noreply.github.com>
2018-05-10 12:04:56 +0200

    Added docstrings

--- src/Transform.py
+++ src/Transform.py
@@ -9,14 +9,29 @@ import numpy as np
 from scipy import *
 
 def transform(matrix, weight = sqrt(2)):
-    W, Winverse = wavelet(matrix, weight, orthagonal = (weight == sqrt(2)))
+    '''
+    Compresses an image ( stored in 'matrix' ) using the Haar Wavelet Transformation method
+    with a certain weight ( deafault sqrt(2) ).
+    Returns the compressed image as a Matrix
+    '''
+    W, Winverse = wavelet(matrix, weight)
     return W * matrix * Winverse
 
 def inverse_transform(matrix, weight = sqrt(2)):
-    W, Winverse = wavelet(matrix, weight, orthagonal = (weight == sqrt(2)))
+    '''
+    Uncompresses an image ( stored in 'matrix' ) using the Haar Wavelet Transformation method
+    with a certain weight ( deafault sqrt(2) ).
+    Returns the uncompressed image as a Matrix
+    '''
+    W, Winverse = wavelet(matrix, weight)
     return Winverse * matrix * W
 
-def wavelet(matrix, weight, orthagonal):
+def wavelet(matrix, weight):
+    '''
+    Constructs the Discrete Haar Wavelet Transformation matrix and its inverse.
+    Depending on the chosen weight ( which determines whether the matrix is orthagonal ),
+    the inverse is either set as the transpose or calculated normally.
+    '''
     W = Matrix(np.zeros(matrix.shape))
     i,j = matrix.indices(W.shape)
     factor = weight/2.
@@ -24,19 +39,29 @@ def wavelet(matrix, weight, orthagonal):
     W[2*i==j-1] = factor
     W[2*i-W.shape[0]==j] = -factor
     W[2*i-W.shape[0]==j-1] = factor
-    if orthagonal:
+    if weight == sqrt(2):
         Winverse = W.transpose()
     else:
         Winverse = W.inverse()
     return W, Winverse
 
 def extransform ( matrix ):
+    '''
+    Compresses an image ( stored in 'matrix' ) using the Haar Wavelet Transformation method.
+    The calculations are made explicitly without the use of matrices.
+    Returns the compressed image as a Matrix
+    '''
     return Matrix( np.array( [ ( matrix[m+1] + matrix[m] ) / 2
                               for m in range( matrix.shape[0] - 1 ) ]
             + [ ( matrix[n+1] - matrix[n] ) / 2 
                for n in range( matrix.shape[0] - 1 ) ] ) )
     
 def exinverse_transform ( matrix ):
+    '''
+    Uncompresses an image ( stored in 'matrix' ) using the Haar Wavelet Transformation method.
+    The calculations are made explicitly without the use of matrices.
+    Returns the uncompressed image as a Matrix
+    '''
     odd = [ matrix[n] - matrix[ n + matrix.shape[0] / 2 ]
     for n in range( matrix.shape[0] / 2 ) ]
     even = [ matrix[n] + matrix[ n + matrix.shape[0] / 2 ]
