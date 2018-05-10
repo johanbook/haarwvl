7f49b9687d3a6b32a4ecaf8fb09adc2c62632f24
bjornannby <39047904+bjornannby@users.noreply.github.com>
2018-05-10 15:33:53 +0200

    fixed dot product

--- src/Matrix.py
+++ src/Matrix.py
@@ -1,49 +1,44 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Apr  9 10:35:12 2018
-
 @author: dvdli
 """
-from  scipy import *
-from  pylab import *
-from  scipy.integrate import quad
 import numpy as np
 
 class Matrix:
 
-
     def __init__(self, array):
+        if not isinstance(array, ndarray):
+            raise TypeError('Expected a numpy array as argument.')
         self.array = array
-        
+        self.shape = self.array.shape
         
     def __add__(self, other):
-        return self.array+other.array
+        return Matrix(self.array+other.array)
         
     def __sub__(self, other):
-        return self.array-other.array
+        return Matrix(self.array-other.array)
         
     def __mul__(self, other):
-        return self.array*other.array
+        return Matrix(dot(self.array,other.array))
         
     def __pow__(self, other):
-        returnself.array**other.array
+        return Matrix(self.array**other.array)
         
     def inverse(self):
-        return np.linalg.inv(self.array)
+        return Matrix(np.linalg.inv(self.array))
         
     def transpose(self):
-        return np.matrix.transpose(self.array)
+        return Matrix(np.matrix.transpose(self.array))
         
-    def __rep__(self):
-        return string(self.array)
-        
-    def shape(self):
-        return np.ndarray.shape(self.array)
+    def __repr__(self):
+        s = ""
+        for x in self.array:
+            s+='\n'+str(x)
+        return s+'\n'    
         
     def indices(self):
-        return np.indices(self.array)
+        return np.indices(self.shape)
         
     def array(self):
-        return self.array
-        
-        
+        return self.array    
