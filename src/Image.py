88f2cdde8e050fb4f2ebb88e90a879de118e2986
Johan Book <johanbook@hotmail.com>
2018-05-10 15:06:25 +0200

    Reformated image class

--- src/Image.py
+++ src/Image.py
@@ -7,40 +7,36 @@ Created on Thu May 10 10:34:56 2018
 
 import os
 import scipy.misc as sm
-from scipy.misc import toimage
-
 
 class Image:
     def __init__(self,path=None):
         '''
         Class representing an grayscale image. Imported images can be compressed using the Haar Wavelet transformation, compressed images can be uncompressed.
         Input: path (string) provided by user, given in one of the two following forms, ex1: 'C:\\users\\documents' or ex2: 'C:/users/documents', if these forms are ignored, nothing will work.
-        '''        
+        '''      
+        
+        // Check if valid path
         if path == None:
             raise Exception('No path was given.')
-        elif not isinstance(path,str):
+        if not isinstance(path, str):
             raise TypeError('The path was not given as a string. Please put citation marks around the path.')
-        elif os.path.exists(path) == False:
+        if not os.path.exists(path):
             raise Exception('Path does not exist. Try again.')
-        self.path = path
+            
+        // Read image and store in class
         self.matrix = Matrix(sm.imread(self.path, True))
         self.reshape()
         
     def reshape(self):
         rows, cols = self.matrix.shape
-        if rows % 2 != 0:
-            self.matrix.array = self.matrix.array[0:-1]
-        if cols % 2 != 0:
-            self.matrix.array = self.matrix.array[:,0:-1]
-        
+        if rows % 2 != 0 : self.matrix.array = self.matrix.array[0:-1]
+        if cols % 2 != 0 : self.matrix.array = self.matrix.array[:,0:-1]
         
     def save(self):
-            sm.imsave('newimagefile.jpg', self.matrix)
+        sm.imsave('newimagefile.jpg', self.matrix.array)
             
     def compress(self,num=1):
-        self.matrix = self.matrix *0
+        self.matrix.array = self.matrix.array*0
     
     def display(self):
-        toimage(self.matrix.array).show()
-    
-    
+        sm.toimage(self.matrix.array).show()
