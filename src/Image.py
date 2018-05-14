62594be6dbcd9d5258d1d8d4372ffd369dc89e20
bjornannby <39047904+bjornannby@users.noreply.github.com>
2018-05-14 11:10:23 +0200

    added compression

--- src/Image.py
+++ src/Image.py
@@ -7,35 +7,77 @@ Created on Thu May 10 10:34:56 2018
 
 import os
 import scipy.misc as sm
+from scipy.misc import toimage
+
 
 class Image:
-    def __init__(self,path):
+    def __init__(self,path=None):
         '''
         Class representing an grayscale image. Imported images can be compressed using the Haar Wavelet transformation, compressed images can be uncompressed.
         Input: path (string) provided by user, given in one of the two following forms, ex1: 'C:\\users\\documents' or ex2: 'C:/users/documents', if these forms are ignored, nothing will work.
-        '''      
+        '''        
         
-        # Check if valid path
-        if not isinstance(path, str):
+        # comments
+        if path == None:
+            raise Exception('No path was given.')
+        elif not isinstance(path,str):
             raise TypeError('The path was not given as a string. Please put citation marks around the path.')
-        if not os.path.exists(path):
+        elif os.path.exists(path) == False:
             raise Exception('Path does not exist. Try again.')
-            
-        # Read image and store in class
+        
+        # comments
+        self.path = path
         self.matrix = Matrix(sm.imread(self.path, True))
         self.reshape()
         
+        self._num = 0 
+        
     def reshape(self):
         rows, cols = self.matrix.shape
-        if rows % 2 != 0 : self.matrix.array = self.matrix.array[0:-1]
-        if cols % 2 != 0 : self.matrix.array = self.matrix.array[:,0:-1]
+        if rows % 2 != 0:
+            self.matrix.array = self.matrix.array[0:-1]
+        if cols % 2 != 0:
+            self.matrix.array = self.matrix.array[:,0:-1]
         self.matrix = Matrix(self.matrix.array)
         
-    def save(self):
-        sm.imsave('newimagefile.jpg', self.matrix.array)
+    def save(self,path):
+            sm.imsave(path, self.matrix.array)
             
-    def compress(self,num=1):
-        self.matrix.array = self.matrix.array*0
+    def compress(self,num=1, echo=False):
+        #self.matrix = transform(self.matrix)
+        self._num = num
+        if num >= 1:
+            for n in range(num):
+                rows, cols = shape(self.matrix.array)
+                #print(rows,cols)
+                temp_matrix = Matrix( self.matrix.array[0:rows/2**(n),0:cols/2**(n)] )
+                
+                if echo:
+                    toimage(temp_matrix.array).show()
+                
+                #print(shape(temp_matrix.array))
+                temp_matrix = transform(temp_matrix)
+                #self.matrix = temp_matrix
+                #toimage(self.invert(temp_matrix).array).show()
+                self.matrix.array[0:rows/2**(n),0:cols/2**(n)] = temp_matrix.array
+                #toimage(self.invert(temp_matrix).array).show()
+                self.matrix = Matrix( self.matrix.array )                
+                
+    def uncompress(self, num=-1):
+        if num < 0:
+            num = self._num
+        
+        
+            
+        self.matrix = inverse_transform(self.matrix)
     
     def display(self):
-        sm.toimage(self.matrix.array).show()
+        toimage(self.matrix.array).show()
+    
+      
+    def invert(self, matrix):
+        X, Y = matrix.shape
+        for x in range(X):
+            for y in range(Y):
+                matrix.array[x][y] = 255 - matrix.array[x][y] 
+        return matrix
